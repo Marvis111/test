4e30e3a7d66f077fa7e3fbe8bd1f3f0073c78d53
Marvis111 <oyegbilegbemiga@gmail.com>
2021-11-03 21:34:53 +0100

    new release

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,5 +2,8 @@
 module.exports = {
     home:(req,res) =>{
         res.render('index',{title:'Home page..'})
+    },
+    login:(req,res)=>{
+        res.render('login',{title:"login page is ready"})
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const indexContoller = require('../controllers/indexController')
 const Router = app =>{
     app.get('/',indexContoller.home);
+    app.get('/login',indexContoller.login);
 }
 module.exports = Router
